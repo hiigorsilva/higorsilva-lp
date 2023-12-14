fd0b4734bde17bd261e5b42227a595d708e3f113
Higor Silva <contato.higordeveloper@gmail.com>
2023-12-14 18:24:52 -0300

    :pencil: add popup on the portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,9 @@
 const btnWhats = document.querySelector(".btn-whats")
-
 // Posição da seção onde o botão do whats será visível
 const secaoBtnVisible = document.querySelector(".target").offsetTop
-
 // Verificar a posição do scrollY e mostrar o botão
 function showBtnWhats() {
   const positionScroll = window.scrollY
-
   // Verifica se a posição do scroll é maior que a posição da seção de visibilidade do botão
   if (positionScroll >= secaoBtnVisible) {
     btnWhats.style.display = "block"
@@ -14,5 +11,29 @@ function showBtnWhats() {
     btnWhats.style.display = "none"
   }
 }
-
 window.addEventListener("scroll", showBtnWhats)
+
+// POPUP PORTFOLIO
+function openPopup(index) {
+  const popup = document.querySelector(".popup-wrapper")
+  const popupImage = document.querySelector("#project")
+  const projectUrl = [
+    "./src/images/project-1-full.jpg",
+    "./src/images/project-2-full.jpg",
+    "./src/images/project-3-full.jpg",
+    "./src/images/project-4-full.jpg",
+  ]
+
+  popupImage.src = projectUrl[index]
+  popup.style.display = "flex"
+}
+
+function closePopup() {
+  const popup = document.querySelector(".popup-wrapper")
+  popup.style.display = "none"
+}
+document
+  .querySelector(".popup-content")
+  .addEventListener("click", function (event) {
+    event.stopPropagation()
+  })
